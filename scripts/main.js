ee5227f0e354d4a1fc56d80eb1dac6fbe29f5fa2
Crash46 <46crash@gmail.com>
2017-04-19 09:59:39 +0300

    lesson 1 finished
    first lesson

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,30 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector('img');
 
-document.querySelector('html').onclick = function() {
-    alert('Stop poking me!');
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/photo.jpg'){
+        myImage.setAttribute ('src','images/photo_2.jpg');
+    } else {
+        myImage.setAttribute('src','images/photo.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'VRA is the best, ' + myName;
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'VRA is the best, ' + storedName
+}
+
+myButton.onclick = function(){
+    setUserName();
 }
